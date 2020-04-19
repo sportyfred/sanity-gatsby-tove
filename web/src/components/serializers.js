0e6ea070f1a9ef2b8703c97f410239c303e5a0d2
sportyfred <ivanboring@gmail.com>
2020-04-19 13:03:54 +0200

    Uppsats ser

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -10,7 +10,27 @@ const serializers = {
     youtube: ({node}) => {
         const { url } = node
         const id = getYouTubeId(url)
-        return (<YouTube videoId={id} />)
+        return ( <div
+      className="video"
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%" /* 16:9 */,
+        paddingTop: 25,
+        height: 0
+      }}
+    >
+      <iframe
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%"
+        }}
+        src={`https://www.youtube.com/embed/${id}`}
+        frameBorder="0"
+      />
+    </div>)
       }
   }
 }
